d15faa3bfbb6c5302eec4f07578278c73f339e83
s21105 <80321755+s21105@users.noreply.github.com>
2021-11-07 21:06:15 +0100

    Create 5d.js

---
+++ 5d.js
@@ -0,0 +1,19 @@
+let student = {
+    firstName: 'Susan',
+    lastName: 'Milton',
+    id: 1,
+    grades: [5, 5, 3, 4, 4, 3],
+    get fullName() {
+        return this.firstName + " " + this.lastName;
+    },
+    set fullName(name) {
+        let word = name.split(' ')
+        this.firsName = word[0]
+        this.lastName = word[1]
+    },
+    get gradesAvg() {
+         return this.grades.reduce((a,b)=>a+b)/this.grades.length;
+    }
+}
+
+console.log(student.fullName, "has an average grade:", student.gradesAvg);
