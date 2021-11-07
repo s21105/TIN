bc929e3db98c6abe05f72c92df9aa76dcb8c2550
s21105 <80321755+s21105@users.noreply.github.com>
2021-11-07 19:06:20 +0100

    Create 5b.js

---
+++ 5b.js
@@ -0,0 +1,17 @@
+function Student(firstName, lastName, id, grades) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.id = id;
+    this.grades = grades;
+
+    this.gradesAvg = function () {
+        return grades.reduce((a,b)=>a+b)/grades.length;
+    }
+
+    this.printsStudentInfo = function () {
+        console.log("Student", firstName, lastName, "has average grade:", this.gradesAvg());
+    }
+}
+
+let student = new Student("Susan", "Milton", 1, [5, 5, 3, 4, 4, 3]);
+student.printsStudentInfo();
