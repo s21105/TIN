2e708cabfb4f8a0b3a087fc4103202d9900d0d4a
s21105 <80321755+s21105@users.noreply.github.com>
2021-11-16 01:42:38 +0100

    Update 6a.js

--- as6/6a.js
+++ as6/6a.js
@@ -1,1 +1,11 @@
-qwqw
+function fromCelsiusToFahrenheit(){
+		var celsius = document.getElementById("celsius").value;
+		var fahrenheit = celsius*9/5 +32;
+		document.getElementById("fahrenheit").value = fahrenheit;
+	}
+
+	function fromFahrenheitToCelsius(){
+		var fahrenheit = document.getElementById("fahrenheit").value;
+		var celsius = (fahrenheit-32)*5/9;
+		document.getElementById("celsius").value = celsius;
+	}
