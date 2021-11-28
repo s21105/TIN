5ba5a4eebbed372c6f27fba522ac36aecd4bb2b9
s21105 <80321755+s21105@users.noreply.github.com>
2021-11-28 19:32:43 +0100

    Update 7b.js

--- as7/7b.js
+++ as7/7b.js
@@ -1,1 +1,13 @@
-efef
+const fs = require('fs')
+const directory = process.argv[2]
+
+fs.watch(directory, (eventType, filename) => {
+  if (filename.endsWith('~')) return
+  console.log(`filename is ${filename}, event type is: ${eventType}`)
+  if (eventType === 'change') {
+    fs.readFile(directory + filename, (err, data) => {
+      if (err) return console.log(err.toString())
+      console.log(data.toString())
+    })
+  }
+})
