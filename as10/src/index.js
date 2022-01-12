acb87bd46ef101a3f6124d3f531ef3343af610f9
s21105 <80321755+s21105@users.noreply.github.com>
2022-01-12 21:53:07 +0100

    Delete as10 directory

--- as10/src/index.js
+++
@@ -1,4 +0,0 @@
-const temp = require('./tempConv.js');
-const dist = require('./distConv.js');
-document.getElementById("b1").addEventListener('click', temp.convertTemp);
-document.getElementById("b2").addEventListener('click', dist.convertDist);
